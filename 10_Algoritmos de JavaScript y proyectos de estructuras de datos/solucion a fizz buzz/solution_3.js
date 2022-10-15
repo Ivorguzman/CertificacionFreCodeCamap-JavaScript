b3ac1ba9654489fb07cb8990bcac4522162a423d
Ivorguzman <66047248+Ivorguzman@users.noreply.github.com>
2022-10-15 02:59:58 -0400

    respaldo 1510202

--- 10_Algoritmos de JavaScript y proyectos de estructuras de datos/solucion a fizz buzz/solution_3.js
+++ 10_Algoritmos de JavaScript y proyectos de estructuras de datos/solucion a fizz buzz/solution_3.js
@@ -1,16 +1,18 @@
+
+/* 
 {
+  // !Ejercicio 1
 
-  let size = 4;
+  {
 
-  let board = "";
-  for (let y = 0; y < size; y++) {
-    console.log("******* y = " + y + " ********");
-    for (let x = 0; x < size; x++) {
-      console.log("------ x = " + x +" --------");
-     //console.log(x);
-      let a = ((x + y) % 2 == 0);
-      console.log(a);
-      if ((x + y) % 2 == 0) {
+    let size = 4;
+
+    let board = "";
+    for (let i = 0; i < size; i++) {
+      console.log("******* i = " + i + " ********");
+      let t_f = ((j + i) % 2 == 0);
+      console.log(t_f);
+      if ((j + i) % 2 == 0) {
         board += "°";
         console.log(board);
       } else {
@@ -18,37 +20,47 @@
         console.log(board);
       }
     };
-    console.log(board += "\n");
-    console.log(board);
   }
 
-  console.log(board);
+  // console.log(board);
 }
 
-/* 
+*/
+
+
+
+
+
 
 {
-  valores = []
-  for (let x = 0; x < 10; x++) {
-    valores.push([
-      2 ** x,
-      2 * x ** 2
-    ])
-  }
-  console.table(valores)
-}
 
-{
-  let size = 10;
-  valores1 = [];
-  valores2 = [];
-  for (let y = 0; y < size; y++) {
-    valores1[y] = 2 ** y
-    for (let x = 0; x < size; x++) {
-      valores2[x] = 2 * x ** 2
+  // !  Ejercicio 2
+
+  {
+    /// Bucle con console.table()
+    valores = [];
+    for (let j = 0; j < 10; j++) {
+      valores.push([
+        2 ** j,
+        2 * j ** 2
+      ]);
     }
+    console.table(valores);
   }
-  console.log(valores1);
-  console.log(valores2);
+
+  {
+    /// bucle con console.log()
+
+    let size = 10;
+    valores1 = [];
+    valores2 = [];
+    for (let i = 0; i < size; i++) {
+      valores1[i] = 2 ** i;
+      for (let j = 0; j < size; j++) {
+        valores2[j] = 2 * j ** 2;
+      }
+    }
+    console.log(valores1);
+    console.log(valores2);
+  }
 }
- */
